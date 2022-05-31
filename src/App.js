89f4ab23bc985b7b2b3bd635cc238c2f3bd7b11b
Eva GC <evarteca@gmail.com>
2022-05-31 22:06:54 +0200

    NavBar sections

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+//! IMPORTS:
+import "./App.css";
+import NavBar from "./components/NavBar";
+import AboutMe from "./components/AboutMe.jsx";
+import Projects from "./components/Projects.jsx";
+import Skills from "./components/Skills.jsx";
+import Training from "./components/Training.jsx";
+import Footer from "./components/Footer.jsx";
 
+//! MAIN FUNCTION:
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <AboutMe />
+      <Projects />
+      <Skills />
+      <Training />
+      <Footer />
     </div>
   );
 }
