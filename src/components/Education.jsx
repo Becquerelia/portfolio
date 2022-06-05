4d164c854430f2ff8c72a49a938429523b6fbcc7
Eva GC <evarteca@gmail.com>
2022-06-05 16:27:00 +0200

    education section

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,9 +1,151 @@
-import React from 'react'
+//!IMPORTS:
+import { Typography } from "@mui/material";
+import {
+  Timeline,
+  TimelineItem,
+  TimelineSeparator,
+  TimelineConnector,
+  TimelineContent,
+  TimelineOppositeContent,
+  TimelineDot,
+} from "@mui/lab";
+import SchoolIcon from "@mui/icons-material/School";
+import AutoStoriesIcon from "@mui/icons-material/AutoStories";
 
+//!MAIN FUNCTION:
 function Education() {
   return (
-    <div>Education</div>
-  )
+    <div
+      style={{
+        paddingTop: "5rem",
+        paddingBottom: "3rem",
+      }}
+    >
+      <Typography gutterBottom variant="h4" component="div" color="#6d11ab">
+        EDUCATION
+      </Typography>
+      <Timeline position="alternate" sx={{ py: 5 }}>
+        <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: "auto 0" }}
+            align="right"
+            variant="body2"
+            color="#6d11ab"
+          >
+            April, 2022
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector />
+            <TimelineDot color="secondary" variant="outlined">
+              <AutoStoriesIcon />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: "12px", px: 2 }}>
+            <Typography variant="h6" component="span" color="#6d11ab">
+              Data Modeling Course (M320)
+            </Typography>
+            <Typography color="#6d11ab"> MongoDB University </Typography>
+          </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: "auto 0" }}
+            variant="body2"
+            color="#6d11ab"
+          >
+            January, 2022 - March, 2022
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector />
+            <TimelineDot color="secondary" variant="outlined">
+              <SchoolIcon />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: "12px", px: 2 }}>
+            <Typography variant="h6" component="span" color="#6d11ab">
+            Full-Stack Web Development Full Time Bootcamp
+            </Typography>
+            <Typography color="#6d11ab">IronHack (Remote)</Typography>
+          </TimelineContent>
+        </TimelineItem>
+
+        <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: "auto 0" }}
+            variant="body2"
+            color="#6d11ab"
+          >
+            October, 2017 - October, 2018
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector />
+            <TimelineDot color="secondary" variant="outlined">
+              <SchoolIcon />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: "12px", px: 2 }}>
+            <Typography variant="h6" component="span" color="#6d11ab">
+            Master in Biotechnology
+            </Typography>
+            <Typography color="#6d11ab">
+            European University (Madrid)
+            </Typography>
+          </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: "auto 0" }}
+            variant="body2"
+            color="#6d11ab"
+          >
+            October, 2016 - July, 2017
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector />
+            <TimelineDot color="secondary" variant="outlined">
+              <SchoolIcon />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: "12px", px: 2 }}>
+            <Typography variant="h6" component="span" color="#6d11ab">
+            Master in Biomedical Science & Research
+            </Typography>
+            <Typography color="#6d11ab">
+            Leon University (Castilla y León)
+            </Typography>
+          </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: "auto 0" }}
+            variant="body2"
+            color="#6d11ab"
+          >
+            September, 2013 - July, 2016
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector />
+            <TimelineDot color="secondary" variant="outlined">
+              <SchoolIcon />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: "12px", px: 2 }}>
+            <Typography variant="h6" component="span" color="#6d11ab">
+            Bachelor’s Degree in Biochemistry
+            </Typography>
+            <Typography color="#6d11ab">
+            Complutense University (Madrid)
+            </Typography>
+          </TimelineContent>
+        </TimelineItem>
+      </Timeline>
+    </div>
+  );
 }
 
-export default Education
+export default Education;
