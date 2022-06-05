ac2105cf19b177d73ab5d5d8c5fb99b228ac7abe
Eva GC <evarteca@gmail.com>
2022-06-05 16:33:25 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,21 @@
+//!IMPORTS:
+import { Typography } from "@mui/material";
 
-
+//!MAIN FUNCTION:
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div
+      style={{
+        backgroundColor: "#6d11ab",
+        paddingTop: "1rem",
+        paddingBottom: "1rem",
+      }}
+    >
+      <Typography gutterBottom variant="h6" component="div" color="white">
+        © EVA G. CAGIGAS, 2022
+      </Typography>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
